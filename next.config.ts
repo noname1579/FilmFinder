d1562d0d0e1e3f9fbca07c504d0ca7ce85bc11c3
noname1579 <ilyalashenko115@gmail.com>
2025-06-30 10:06:36 +0300

    добавил gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/FilmFinder/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/FilmFinder' : ''
 };
 
 export default nextConfig;
